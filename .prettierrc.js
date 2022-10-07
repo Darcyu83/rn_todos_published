a720158ce27fdf02dacccfb7e0e8278e96f26f06
Darcy <darcy.u83@gmail.com>
2022-10-07 14:59:51 +0900

    eslint, prettier set up with airbnb

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  "singleQuote": true,
+  "parser": "typescript",
+  "semi": true,
+  "useTabs": true,
+  "printWidth": 120,
+  "tabWidth": 2,
 };
---
+++ src/screens/todos/TodosMainScrn.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+interface IProps {}
+
+function TodosMainScrn({}: IProps) {
+	return (
+		<View style={{}}>
+			<Text>TodosMainScrn</Text>
+		</View>
+	);
+}
+
+export default TodosMainScrn;
